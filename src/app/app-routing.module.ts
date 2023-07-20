da127786503a6bc030409de9e8e1eed91a97c3fa
katharinahamburg <katharina@hinnrichs.de>
2023-07-20 15:05:28 +0200

    display card stack and start game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
+  {path: 'game', component: GameComponent},
   {path: '', component: StartScreenComponent},
 ];
 
