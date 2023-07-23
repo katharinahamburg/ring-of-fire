402db25618a88450c2609d644c273e9575c249e2
katharinahamburg <katharina@hinnrichs.de>
2023-07-23 14:16:10 +0200

    card animated

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent {
+  pickCardAnimation = false;
 
+  takeCard() {
+    this.pickCardAnimation = true;
+
+  }
 }
