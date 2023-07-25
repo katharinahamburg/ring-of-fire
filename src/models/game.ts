6cd336188fd49e6975630b5da55597c08e8335c0
katharinahamburg <katharina@hinnrichs.de>
2023-07-25 13:03:56 +0200

    Spieler hinzufügen

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = ['Monika', 'Anne', 'Paula'];
+    public players: string[] = [];
     public stack: string[] = [];
     public playedCards: string [] = [];
     public currentPlayer: number = 0;
